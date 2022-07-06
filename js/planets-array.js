b0913a3b5b302b61cdbe18ccb4ff6313fd4d338f
NatalieSneed <106278703+NatalieSneed@users.noreply.github.com>
2022-07-05 22:04:52 -0500

    exercise updates and review

--- js/planets-array.js
+++ js/planets-array.js
@@ -19,8 +19,9 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
-    console.log(planets);
     planets.unshift('The Sun');
+    console.log(planets);
+
 
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
@@ -36,8 +37,8 @@
 
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
-    console.log("Where's the earth: ? + planets.indexOf("Earth");
-    console.log("Here's the lastIndexOf finding the Earth " + planets.lastIndexOf("Earth");
+    console.log("Where's the earth: ?" + planets.indexOf("Earth"));
+    console.log("Here's the lastIndexOf finding the Earth " + planets.lastIndexOf("Earth"));
 
     console.log("Reversing the order of the planets array.");
     planets.reverse();
--- js/while.js
+++ js/while.js
@@ -1,22 +1,31 @@
 "use strict";
-//
-// 2
-// 4
-// 8
-// 16
-// 32
-// 64
-// 128
-// 256
-// 512
-// 1024
-// 2048
-// 4096
-// 8192
-// 16384
-// 32768
-// 65536
+
 
 // Create a while loop that uses console.log() to create the output shown below.
 
-var i = 2;
+let i = 1;
+
+    while(i < 65536);{
+    i = i * 2; //i *= 2;
+    console.log(i);
+}
+
+// An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. The below code shows how to get the random numbers for this exercise.
+
+// This is how you get a random number between 50 and 100
+// Math.floor(Math.random() * (max - min) ) + min;
+
+// This expression will generate a random number between 1 and 5
+// Math.floor(Math.random() * 5) + 1;
+
+let allCones = Math.floor(Math.random() * (100 - 50) ) + 50;
+    do {
+        let conesSold = Math.floor(Math.random() * 5) + 1;
+    //    amount of cones being bought by her clients
+        if(allCones >= conesSold) {
+            allCones = allCones - conesSold;
+            console.log(conesSold + " cones have sold." + allCones);
+        } else {
+           console.log("there are only " + allCones + "left over " + conesSold + " .");
+    }
+}while (allCones !=0);
